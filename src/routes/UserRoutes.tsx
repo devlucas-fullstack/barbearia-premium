a44903588764142f4f87b204d44700bbdcddabaa
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-27 16:10:43 -0300

    feat: creates user panel layout

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import { AppLayout } from "../components/AppLayout";
 import { UserDashboard } from "../pages/UserDashboard";
 
 export function UserRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<UserDashboard />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<UserDashboard />} />
+      </Route>
     </Routes>
   );
 }
