69bdd44800df404afd66a2953630a81a499606ba
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-27 18:03:37 -0300

    feat: creates select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,20 @@
+type Props = React.ComponentProps<"select"> & {
+  legend?: string;
+};
+
+export function Select({ legend, children, ...rest }: Props) {
+  return (
+    <fieldset>
+      <legend className="block text-sm text-slate-700 mb-2">{legend}</legend>
+      <select
+        className="w-full px-4 py-2 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500"
+        {...rest}
+      >
+        <option disabled hidden value="">
+          Selecione
+        </option>
+        {children}
+      </select>
+    </fieldset>
+  );
+}
