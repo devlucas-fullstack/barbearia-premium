e07d492a0f8538f7b773e97099eef66750978ef1
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-27 14:27:02 -0300

    feat: creates login interface

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,20 @@
+import { Input } from "../components/Input";
+import { Button } from "../components/Button";
+import { Link } from "react-router-dom";
+
 export function SignIn() {
-  return <h1>signin</h1>;
+  return (
+    <form className="bg-white rounded-lg p-8 shadow-lg flex flex-col gap-6">
+      <Input legend="Email" type="email" />
+      <Input legend="Senha" type="password" />
+      <Button type="submit">Entrar</Button>
+
+      <Link
+        to="/signup"
+        className="text-slate-400 hover:text-slate-500 text-sm text-center"
+      >
+        Criar conta
+      </Link>
+    </form>
+  );
 }
