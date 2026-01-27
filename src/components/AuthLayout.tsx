68a909715e7475f3dcb93a5674928ddacf28b9e7
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-27 13:46:48 -0300

    feat: creates authentication layout

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import logo from "../assets/logo.svg";
+
+export function AuthLayout() {
+  return (
+    <div className="h-screen w-screen bg-purple-950 flex justify-center items-center p-4">
+      <main className="w-full max-w-md">
+        <div className="text-center mb-8">
+          <img src={logo} alt="Logo" className="w-16 h-16 mx-auto mb-4" />
+          <h1 className="text-3xl text-white mb-2 text-center">
+            Barbearia Premium
+          </h1>
+          <p className="text-slate-400 text-center">Sistema de Agendamento</p>
+        </div>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
