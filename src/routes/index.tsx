72a83c811e5cffebb0a9af49aea0abd543428ee3
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-29 12:16:58 -0300

    feat: redirects according to the role

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,26 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthRoutes } from "./AuthRoutes";
 import { UserRoutes } from "./UserRoutes";
 import { AdminRoutes } from "./AdminRoutes";
+import { useAuth } from "../hooks/auth";
 
 export function Routes() {
+  const { session } = useAuth();
+
+  function AppRoutes() {
+    switch (session?.user.role) {
+      case "ADMIN":
+      case "BARBER":
+        return <AdminRoutes />;
+      case "CLIENT":
+        return <UserRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
   return (
     <BrowserRouter>
-      <AdminRoutes />
+      <AppRoutes />
     </BrowserRouter>
   );
 }
