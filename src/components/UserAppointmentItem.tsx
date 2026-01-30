d2ff3782a4861d9440a33901535018ff23fc30eb
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-30 14:23:21 -0300

    feat: displays formatted date and time

--- src/components/UserAppointmentItem.tsx
+++ src/components/UserAppointmentItem.tsx
@@ -1,11 +1,15 @@
 import { Calendar, Clock } from "lucide-react";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-type AppointmentItemProps = {
+export type AppointmentItemProps = {
   id: string;
-  barber: string;
+  barbeiro: { nome: string };
+  cliente: { nome: string };
   status: string;
-  category: string;
-  date: string;
+  servico: string;
+  data: string;
+  criado_em: string;
 };
 
 type Props = React.ComponentProps<"div"> & {
@@ -13,28 +17,37 @@ type Props = React.ComponentProps<"div"> & {
 };
 
 export function UserAppointmentItem({ data, ...rest }: Props) {
+  const date = new Date(data.data);
+
+  const dataFormat = format(date, "dd/MM/yyyy", { locale: ptBR });
+  const horaFormat = format(date, "HH:mm", { locale: ptBR });
+
   return (
     <div className="bg-white rounded-lg p-6 border border-slate-200" {...rest}>
       <div className="flex items-start justify-between mb-4">
         <div>
-          <h3 className="text-slate-900 mb-1">{data.category}</h3>
-          <p className="text-sm text-slate-500">Barbeiro: {data.barber}</p>
+          <h3 className="text-slate-900 mb-1">{data.servico}</h3>
+          <p className="text-sm text-slate-500">
+            Barbeiro: {data.barbeiro.nome}
+          </p>
         </div>
-        <span className="px-3 py-1 rounded-full text-xs bg-green-100 text-green-800">
+        <span
+          className={`px-3 py-1 rounded-full text-xs  ${data.status === "Confirmado" ? "bg-green-100 text-green-800" : ""} ${data.status === "Pendente" ? "bg-yellow-100 text-yellow-800" : ""} ${data.status === "Cancelado" ? "bg-red-100 text-red-800" : ""}`}
+        >
           {data.status}
         </span>
       </div>
       <div className="flex items-center gap-4 text-sm text-slate-600 mb-4">
         <div className="flex items-center gap-2">
           <Calendar className="w-4 h-4" />
-          {data.date}
+          {dataFormat}
         </div>
         <div className="flex items-center gap-2">
           <Clock className="w-4 h-4" />
-          {data.date}
+          {horaFormat}
         </div>
       </div>
-      <button className="w-full px-4 py-2 bg-red-50 hover:bg-red-100 text-red-600 rounded-lg trasition-color text-sm cursor-pointer">
+      <button className="w-full px-4 py-2 bg-red-50 hover:bg-red-100 text-red-600 rounded-lg trasition-colors text-sm cursor-pointer">
         Cancelar Agendamento
       </button>
     </div>
